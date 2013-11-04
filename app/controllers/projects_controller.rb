545b0801eb1454ecb1676113422c709e4c65c936
Timothy Barnes <barnes7td@gmail.com>
2013-11-04 11:58:05 -0500

    Add feature to allow for creating new Projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,6 +7,15 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
   end
 
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.create params[:project]
+    redirect_to projects_path
+  end
+
   def ready
     @projects = Project.order(drawings: s)
   end

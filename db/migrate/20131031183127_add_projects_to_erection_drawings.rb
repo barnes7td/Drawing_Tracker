cada25fc8704d3b77e20ef143b408d5eb09c2a33
Timothy Barnes <barnes7td@gmail.com>
2013-11-04 11:40:46 -0500

    remove erection drawing migrations

--- db/migrate/20131031183127_add_projects_to_erection_drawings.rb
+++
@@ -1,6 +0,0 @@
-class AddProjectsToErectionDrawings < ActiveRecord::Migration
-  def change
-    add_column :erection_drawings, :project_id, :integer
-    add_index  :erection_drawings, :project_id
-  end
-end
--- db/migrate/20131031184302_add_status_to_erection_drawings.rb
+++
@@ -1,5 +0,0 @@
-class AddStatusToErectionDrawings < ActiveRecord::Migration
-  def change
-    add_column :erection_drawings, :status, :string
-  end
-end
